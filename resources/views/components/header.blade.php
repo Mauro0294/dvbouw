9d106ba2296dfb97fb196db0b89efac33a08e2be
Mauro Scheltens <mauroscheltens@hotmail.com>
2023-06-19 22:02:44 +0200

    feat: nav animations

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,17 +2,17 @@
     <div class="flex justify-between items-center container">
         <a href="{{ route('home') }}"><img src="{{ asset('images/logo.png') }}" class="w-20 py-2"></a>
         <ul class="hidden lg:flex text-lg gap-16 font-bold py-8">
-            <a href="{{ route('home') }}"><li>HOME.</li></a>
-            <a href="{{ route('home') }}#diensten"><li>DIENSTEN.</li></a>
-            <a href="{{ route('home') }}#contact"><li>CONTACT.</li></a>
-            <a href="{{ route('ons-werk') }}"><li>ONS WERK.</li></a>
-            <a href="https://www.facebook.com/p/DV-Onderhoud-100064673394829/" target="_blank" rel="noreferrer" class="mt-1"><li><img src="{{ asset('images/facebook.png') }}" class="w-6 h-6"></li></a>
+            <a href="{{ route('home') }}" data-aos="fade-down" data-aos-delay="100"><li>HOME.</li></a>
+            <a href="{{ route('home') }}#diensten" data-aos="fade-down" data-aos-delay="200"><li>DIENSTEN.</li></a>
+            <a href="{{ route('home') }}#contact" data-aos="fade-down" data-aos-delay="300"><li>CONTACT.</li></a>
+            <a href="{{ route('ons-werk') }}"data-aos="fade-down" data-aos-delay="400"><li>ONS WERK.</li></a>
+            <a href="https://www.facebook.com/p/DV-Onderhoud-100064673394829/"data-aos="fade-down" data-aos-delay="500" target="_blank" rel="noreferrer" class="mt-1"><li><img src="{{ asset('images/facebook.png') }}" class="w-6 h-6"></li></a>
         </ul>
         <div class="block lg:hidden">
             <button id="hamburger-btn" class="hamburger-btn">
-                <span class="hamburger-line"></span>
-                <span class="hamburger-line"></span>
-                <span class="hamburger-line"></span>
+                <span class="hamburger-line" data-aos="fade-down" data-aos-delay="400"></span>
+                <span class="hamburger-line" data-aos="fade-down" data-aos-delay="200"></span>
+                <span class="hamburger-line" data-aos="fade-down"></span>
             </button>
         </div>
     </div>
