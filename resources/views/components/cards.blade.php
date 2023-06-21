12abdba993f551b18d371eb14c49bcd1dac64f05
Mauro Scheltens <mauroscheltens@hotmail.com>
2023-06-21 19:58:48 +0200

    feat: data connected to HyGraph headless cms

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -3,12 +3,12 @@
         <h3 class="text-sm text-white">Waar wij mee kunnen helpen</h3>
         <div class="w-12 h-[2px] bg-[#D2153D] my-5"></div>
         <div id="cards" class="grid lg:grid-cols-3 lg:justify-between gap-12 lg:gap-24 uppercase">
-            @foreach ($categories as $key=>$category)
+            @foreach ($diensten as $key=>$dienst)
             <a href="{{ route('home') }}#contact" data-aos="fade-up" data-aos-delay="{{ $key }}00">
                 <div id="card" class="relative">
-                    <img src="{{ asset('/images/diensten/' . $category->img_src . '') }}" class="w-full">
+                    <img src="{{ $dienst['image']['url'] }}" class="w-full">
                     <div class="bg-black text-white absolute top-0 w-full py-1">
-                        <p class="text-center text-xl font-bold">{{ $category->name }}</p>
+                        <p class="text-center text-xl font-bold">{{ $dienst['naam'] }}</p>
                     </div>
                     <button class="absolute bg-black text-white left-0 right-0 top-0 bottom-0 m-auto h-fit w-fit px-3 py-1">Offerte aanvragen</button>
                 </div>
--- resources/views/components/three_columns.blade.php
+++ resources/views/components/three_columns.blade.php
@@ -1,19 +1,11 @@
 <section class="container text-white !py-12" id="three_columns">
     <div class="lg:flex justify-between">
-        <div class="mb-8 lg:mb-0 flex-0" data-aos="zoom-in-up">
-            <img src="{{ asset('images/price.svg') }}" class="w-16 h-16 lg:w-20 lg:h-20 mx-auto mb-2">
-            <h3 class="text-2xl font-bold text-center">Beste prijs</h3>
-            <p class="text-[#A5A7AA] text-center w-2/3 mx-auto lg:max-w-[500px]">Bij DVBouw bieden we de beste diensten tegen concurrerende tarieven aan.</p>
-        </div>
-        <div class="mb-8 lg:mb-0 flex-0" data-aos="zoom-in-up" data-aos-delay="200">
-            <img src="{{ asset('images/trusted.svg') }}" class="w-16 h-16 lg:w-20 lg:h-20 mx-auto mb-2">
-            <h3 class="text-2xl font-bold text-center">Betrouwbaar & ervaren bedrijf</h3>
-            <p class="text-[#A5A7AA] text-center w-2/3 mx-auto lg:max-w-[500px]">Al jaren lang een vertrouwde en ervaren partner voor al uw behoeften.</p>
-        </div>
-        <div class="mb-8 lg:mb-0 flex-0" data-aos="zoom-in-up" data-aos-delay="400">
-            <img src="{{ asset('images/quality.svg') }}" class="w-16 h-16 lg:w-20 lg:h-20 mx-auto mb-2">
-            <h3 class="text-2xl font-bold text-center">Goede kwaliteit</h3>
-            <p class="text-[#A5A7AA] text-center w-2/3 mx-auto lg:max-w-[500px]">Ons toegewijde team van professionals heeft een passie voor vakmanschap en kwaliteit.</p>
-        </div>
+        @foreach ($kernwaardes as $key=>$kernwaarde)
+            <div class="mb-8 lg:mb-0 flex-0" data-aos="zoom-in-up" data-aos-delay="{{ $key }}00">
+                <img src="{{ $kernwaarde['icon']['url'] }}" class="w-16 h-16 lg:w-20 lg:h-20 mx-auto mb-2">
+                <h3 class="text-2xl font-bold text-center">{{ $kernwaarde['heading'] }}</h3>
+                <p class="text-[#A5A7AA] text-center w-2/3 mx-auto lg:max-w-[500px]">{{ $kernwaarde['description'] }}</p>
+            </div>
+        @endforeach
     </div>
 </section>
